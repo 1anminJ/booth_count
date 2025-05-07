9e68a56e7222494292896c417cde05e224478786
Seungwan Yoo <ysw99@kakao.com>
2025-05-07 11:12:29 +0900

    feat: Admin model 추가

--- models.py
+++ models.py
@@ -20,6 +20,12 @@ class Log(db.Model):
     identity = db.Column(db.Enum(IdentityEnum), nullable=False)
     timestamp = db.Column(db.DateTime, nullable=False, default=datetime.astimezone(datetime.now()))
 
+class Admin(db.Model):
+    __tablename__ = 'admins'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    phone_number = db.Column(db.String(20), nullable=False, unique=True)
+    created_at = db.Column(db.DateTime, nullable=False, default=datetime.astimezone(datetime.now()))
+
 def create_db(app):
     db.init_app(app)
     with app.app_context():
