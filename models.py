3aea20869e85bc8c010793bbd097bfbfe0734b9c
Seungwan Yoo <ysw99@kakao.com>
2025-05-15 00:34:29 +0900

    feat: 서비스 세팅 관리 모델 추가

--- models.py
+++ models.py
@@ -26,6 +26,17 @@ class Admin(db.Model):
     phone_number = db.Column(db.String(20), nullable=False, unique=True)
     created_at = db.Column(db.DateTime, nullable=False, default=datetime.astimezone(datetime.now()))
 
+class SystemSetting(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    key = db.Column(db.String(50), unique=True, nullable=False)
+    value = db.Column(db.String(255), nullable=False)
+    updated_at = db.Column(db.DateTime, default=datetime.utcnow)
+
+    @classmethod
+    def get_value(cls, key, default=None):
+        setting = cls.query.filter_by(key=key).first()
+        return setting.value if setting else default
+
 def create_db(app):
     db.init_app(app)
     with app.app_context():
